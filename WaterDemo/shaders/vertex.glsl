9caae8335bf0a68aa7b4bd1e024bb304683bc5e6
AidenKerr <arkerr2000@gmail.com>
2023-07-26 14:07:31 -0700

    add exponential waves

--- WaterDemo/shaders/vertex.glsl
+++ WaterDemo/shaders/vertex.glsl
@@ -16,12 +16,14 @@ uniform float time;
 out vec3 FragPos;
 out vec3 Normal;
 
+float sinWave(vec3 pos, Wave wave);
+float sinWaveDerivative(vec3 pos, Wave wave);
 float wave(vec3 pos, Wave wave);
 float waveDerivative(vec3 pos, Wave wave);
 
-Wave waves[] = Wave[](Wave(normalize(vec3(1.0, 0.0, 0.3)), 1.0, 1.0),
-                      Wave(normalize(vec3(0.3, 0.0, -0.9)), 0.8, 2.0),
-                      Wave(normalize(vec3(-0.9, 0.0, 0.2)), 0.5, 3.0),
+Wave waves[] = Wave[](Wave(normalize(vec3(1.0, 0.0, 0.3)), 0.5, 1.0),
+                      Wave(normalize(vec3(0.3, 0.0, -0.9)), 0.4, 2.0),
+                      Wave(normalize(vec3(-0.9, 0.0, 0.2)), 0.3, 3.0),
                       Wave(normalize(vec3(-0.5, 0.0, -0.8)), 0.2, 4.0));
 const int wavesCount = waves.length();
 
@@ -42,12 +44,22 @@ void main()
     FragPos = vec3(model * vec4(pos, 1.0));
 }
 
-float wave(vec3 pos, Wave w)
+float sinWave(vec3 pos, Wave w)
 {
     return w.amp * sin(dot(w.dir, pos) + time * w.speed);
 }
 
-float waveDerivative(vec3 pos, Wave w)
+float sinWaveDerivative(vec3 pos, Wave w)
 {
     return w.amp * cos(dot(w.dir, pos) + time * w.speed);
+}
+
+float wave(vec3 pos, Wave w)
+{
+    return w.amp * exp(sin(dot(w.dir, pos) + time * w.speed));
+}
+
+float waveDerivative(vec3 pos, Wave w)
+{
+    return w.amp * exp(sin(dot(w.dir, pos) + time * w.speed)) * cos(dot(w.dir, pos) + time * w.speed);
 }
